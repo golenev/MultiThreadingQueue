0abbd232fafd8b26c234720f4a6656a70600c018
Basile <interbeing34@gmail.com>
2025-03-09 22:00:32 +0300

    После всех экспериментов, стало понятно, что нельзя синхронизировать provideArguments() и afterEachCallBack(). Поэтому в котлин примере функция afterEach() вынесена в класс с тестов, а в джава примрее оставил нерабочий вариант для пример, в надежде, что кто-то придумат решение

--- src/test/java/javaTests/MainTest.java
+++ src/test/java/javaTests/MainTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
-@Execution(ExecutionMode.CONCURRENT)
 public class MainTest {
 
     private Office office;
