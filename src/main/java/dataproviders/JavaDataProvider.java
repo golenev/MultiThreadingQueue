813cfe7017d1ce9e7864163aa2ac5e176db6a4e1
Basile <interbeing34@gmail.com>
2025-03-17 21:03:48 +0300

    fix old dataprovider

--- src/main/java/dataproviders/JavaDataProvider.java
+++ src/main/java/dataproviders/JavaDataProvider.java
@@ -8,13 +8,9 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Stream;
 
 public class JavaDataProvider implements ArgumentsProvider {
-
-    public static final ExtensionContext.Namespace NAMESPACE
-            = ExtensionContext.Namespace.create(JavaDataProvider.class);
 
     public Office createOffice(int origin, int bound) {
         Faker faker = new Faker();
@@ -27,41 +23,18 @@ public class JavaDataProvider implements ArgumentsProvider {
 
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
-        final ConcurrentLinkedQueue<Office> officeQueue = new ConcurrentLinkedQueue<>();
-        String testKey = context.getRequiredTestMethod().getName();
-        List<Office> offices = List.of(
+        List<Arguments> arguments = List.of(
                 /**
                  * тут мы имитируем поведение тест кейсов,
                  * которые что-то делают на бекенде,
                  * каждый разное время
                  */
-                createOffice(200, 800),
-                createOffice(1000, 2000),
-                createOffice(3500, 6000),
-                createOffice(8000, 10000)
+                Arguments.of(createOffice(4000, 4001)),
+                Arguments.of(createOffice(4000, 4001)),
+                Arguments.of(createOffice(4000, 4001)),
+                Arguments.of(createOffice(4000, 4001))
         );
-        List<Arguments> arguments = offices.stream()
-                .map(o -> {
-                    officeQueue.add(o);
-                    return Arguments.of(o);
-                })
-                .toList();
-        context.getStore(NAMESPACE).put(testKey, officeQueue);
         return arguments.stream();
     }
 
-    /**
-     * тут был afterEach который нарушал порядок
-     */
-//    @Override
-//    public void afterEach(ExtensionContext context) {
-//        String testKey = context.getRequiredTestMethod().getName();
-//        ConcurrentLinkedQueue<Office> officeQueue =
-//                context.getStore(NAMESPACE).get(testKey, ConcurrentLinkedQueue.class);
-//
-//        if (officeQueue != null && !officeQueue.isEmpty()) {
-//            Office office = officeQueue.remove();
-//            new Queries().deleteOfficeById(office.getOfficeId());
-//        } else throw new IllegalStateException("officesQueue is null or empty");
-//    }
 }
